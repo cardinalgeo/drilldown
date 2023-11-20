1bbde3b16f5b799870b1e7521b14e0a2cc200429
Robert Collar <rscollar@gmail.com>
2023-11-20 15:32:33 -0500

    add methods to DrillholeGroup class for creating and show collar, survey, and interval meshes

--- drilldown/holes.py
+++ drilldown/holes.py
@@ -3,7 +3,8 @@ from geoh5py.workspace import Workspace
 from geoh5py.groups import DrillholeGroup
 from geoh5py.objects import Drillhole
 import numpy as np
-from plotter import DrillDownPlotter
+
+# from plotter import DrillDownPlotter
 
 
 class DrillHoleGroup:
@@ -27,30 +28,31 @@ class DrillHoleGroup:
             hole.add_collar(self.collars[self.collars[:, 0] == hole_id, 1:][0])
 
             surveys = np.hsplit(self.surveys[self.surveys[:, 0] == hole_id, 1:], 3)
-            hole.add_survey(surveys[0], surveys[1], surveys[2])
+            if (surveys[0].shape[0]) > 0:
+                hole.add_survey(surveys[0], surveys[1], surveys[2])
 
-            hole.create_hole()
+                hole.create_hole()
 
-            self._holes[hole_id] = hole
+                self._holes[hole_id] = hole
 
-    def add_from_to(self, hole_id, from_to):
-        self.from_to = np.c_[hole_id, from_to]
-
-        for hole_id in np.unique(hole_id):
-            self._holes[hole_id].add_from_to(
-                self.from_to[self.from_to[:, 0] == hole_id, 1:]
-            )
+    def add_from_to(self, hole_ids, from_to):
+        self.from_to = np.c_[hole_ids, from_to]
+        hole_ids = [id for id in np.unique(hole_ids) if id in self._holes.keys()]
+        for id in hole_ids:
+            self._holes[id].add_from_to(self.from_to[self.from_to[:, 0] == id, 1:])
 
         return self.from_to
 
-    def add_data(self, name, hole_id, data):
-        self.hole_ids_with_data = np.unique(hole_id)
-        data = np.c_[hole_id, data]
+    def add_data(self, name, hole_ids, data):
+        self.hole_ids_with_data = np.unique(hole_ids)
+        data = np.c_[hole_ids, data]
         self.vars.append(name)
+
+        hole_ids = [id for id in np.unique(hole_ids) if id in self._holes.keys()]
         data_added = {}
-        for hole_id in self.hole_ids_with_data:
-            data_added[hole_id] = self._holes[hole_id].add_data(
-                name, data[:, 1][data[:, 0] == hole_id]
+        for id in hole_ids:
+            data_added[id] = self._holes[id].add_data(
+                name, data[:, 1][data[:, 0] == id]
             )
         return data_added
 
@@ -64,17 +66,64 @@ class DrillHoleGroup:
 
         return self.polydata
 
-    def show_collars(self):
+    def make_collars_mesh(self):
+        mesh = pv.PolyData(np.asarray(self.collars[:, 1:], dtype="float"))
+
+        return mesh
+
+    def make_surveys_mesh(self):
+        mesh = None
+        for hole_id in self._holes.keys():
+            hole = self._holes[hole_id]
+            depths = hole.desurvey()
+            from_to = hole.make_from_to(depths)
+            if from_to.shape[0] > 0:
+                if mesh is None:
+                    mesh = hole._make_line_mesh(from_to[:, 0], from_to[:, 1])
+                else:
+                    mesh += hole._make_line_mesh(from_to[:, 0], from_to[:, 1])
+
+        return mesh
+
+    def make_intervals_mesh(self, name):
+        meshes = None
+        for hole_id in self._holes.keys():
+            hole = self._holes[hole_id]
+            from_depths = hole.desurvey(hole.from_to[:, 0])
+            to_depths = hole.desurvey(hole.from_to[:, 1])
+            if from_depths.shape[0] > 0:
+                mesh = hole._make_line_mesh(from_depths, to_depths)
+                for var in self.vars:
+                    mesh.cell_data[var] = hole._hole.get_data(var)[0].values
+                if meshes is None:
+                    meshes = mesh
+                else:
+                    meshes += mesh
+
+        return meshes
+
+    def make_points_mesh(self):
         return
+
+    def show_collars(self):
+        mesh = self.make_collars_mesh()
+
+        return mesh.plot()
 
     def show_surveys(self):
-        return
+        mesh = self.make_surveys_mesh()
 
-    def show_intervals(self):
-        return
+        return mesh.plot()
 
-    def show_points(self):
-        return
+    def show_intervals(self, name=None):
+        mesh = self.make_intervals_mesh()
+
+        return mesh.plot()
+
+    def show_points(self, name=None):
+        mesh = self.make_surveys_mesh(name)
+
+        return mesh.plot()
 
 
 class DrillHole:
@@ -183,8 +232,8 @@ class DrillHole:
         return mesh
 
     def make_intervals_mesh(self, name):
-        from_depths = self.desurvey(self.from_to[:,0])
-        to_depths = self.desurvey(self.from_to[:,1])
+        from_depths = self.desurvey(self.from_to[:, 0])
+        to_depths = self.desurvey(self.from_to[:, 1])
         mesh = self._make_line_mesh(from_depths, to_depths)
 
         for var in self.vars:
