a66dc0fe3c62020cccfdcf944ff5d1002dfbe94e
Robert Collar <rscollar@gmail.com>
2024-02-06 20:53:57 -0500

    add plotly plots for filtered data and all data to `Plotly2dMixin`

--- drilldown/plot/plotting_mixin.py
+++ drilldown/plot/plotting_mixin.py
@@ -2,6 +2,16 @@ class Plotting2dMixin:
     def __init__(self):
         pass
 
+    def scatter_plot(self, x, y, **kwargs):
+        from .plotly_plots import ScatterPlot
+
+        fig = ScatterPlot(self.all_data, x, y, **kwargs)
+
+        fig.layer = self
+        fig.ids = self.all_ids
+
+        return fig
+
     def selected_scatter_plot(self, x, y, **kwargs):
         from .plotly_plots import ScatterPlot
 
@@ -12,6 +22,26 @@ class Plotting2dMixin:
 
         return fig
 
+    def filtered_scatter_plot(self, x, y, **kwargs):
+        from .plotly_plots import ScatterPlot
+
+        fig = ScatterPlot(self.filtered_data, x, y, **kwargs)
+
+        fig.layer = self
+        fig.ids = self.filtered_ids
+
+        return fig
+
+    def scatter_3d_plot(self, x, y, z, **kwargs):
+        from .plotly_plots import Scatter3dPlot
+
+        fig = Scatter3dPlot(self.all_data, x, y, z, **kwargs)
+
+        fig.layer = self
+        fig.ids = self.all_ids
+
+        return fig
+
     def selected_scatter_3d_plot(self, x, y, z, **kwargs):
         from .plotly_plots import Scatter3dPlot
 
@@ -19,6 +49,26 @@ class Plotting2dMixin:
 
         fig.layer = self
         fig.ids = self.selected_ids
+
+        return fig
+
+    def filtered_scatter_3d_plot(self, x, y, z, **kwargs):
+        from .plotly_plots import Scatter3dPlot
+
+        fig = Scatter3dPlot(self.filtered_data, x, y, z, **kwargs)
+
+        fig.layer = self
+        fig.ids = self.filtered_ids
+
+        return fig
+
+    def scatter_ternary_plot(self, a, b, c, **kwargs):
+        from .plotly_plots import ScatterTernaryPlot
+
+        fig = ScatterTernaryPlot(self.all_data, a, b, c, **kwargs)
+
+        fig.layer = self
+        fig.ids = self.all_ids
 
         return fig
 
@@ -34,6 +84,28 @@ class Plotting2dMixin:
 
         return fig
 
+    def filtered_scatter_ternary_plot(self, a, b, c, **kwargs):
+        from .plotly_plots import ScatterTernaryPlot
+
+        fig = ScatterTernaryPlot(self.filtered_data, a, b, c, **kwargs)
+
+        fig.plotter = self
+
+        fig.layer = self
+        fig.ids = self.filtered_ids
+
+        return fig
+
+    def scatter_dimensions_plot(self, dimensions, **kwargs):
+        from .plotly_plots import ScatterDimensionsPlot
+
+        fig = ScatterDimensionsPlot(self.all_data, dimensions, **kwargs)
+
+        fig.layer = self
+        fig.ids = self.all_ids
+
+        return fig
+
     def selected_scatter_dimensions_plot(self, dimensions, **kwargs):
         from .plotly_plots import ScatterDimensionsPlot
 
@@ -43,6 +115,28 @@ class Plotting2dMixin:
 
         fig.layer = self
         fig.ids = self.selected_ids
+
+        return fig
+
+    def filtered_scatter_dimensions_plot(self, dimensions, **kwargs):
+        from .plotly_plots import ScatterDimensionsPlot
+
+        fig = ScatterDimensionsPlot(self.filtered_data, dimensions, **kwargs)
+
+        fig.plotter = self
+
+        fig.layer = self
+        fig.ids = self.filtered_ids
+
+        return fig
+
+    def bar_plot(self, x, y, **kwargs):
+        from .plotly_plots import BarPlot
+
+        fig = BarPlot(self.all_data, x, y, **kwargs)
+
+        fig.layer = self
+        fig.ids = self.all_ids
 
         return fig
 
@@ -58,6 +152,28 @@ class Plotting2dMixin:
 
         return fig
 
+    def filtered_bar_plot(self, x, y, **kwargs):
+        from .plotly_plots import BarPlot
+
+        fig = BarPlot(self.filtered_data, x, y, **kwargs)
+
+        fig.plotter = self
+
+        fig.layer = self
+        fig.ids = self.filtered_ids
+
+        return fig
+
+    def histogram(self, x, **kwargs):
+        from .plotly_plots import Histogram
+
+        fig = Histogram(self.all_data, x, **kwargs)
+
+        fig.layer = self
+        fig.ids = self.all_ids
+
+        return fig
+
     def selected_histogram(self, x, **kwargs):
         from .plotly_plots import Histogram
 
@@ -67,5 +183,17 @@ class Plotting2dMixin:
 
         fig.layer = self
         fig.ids = self.selected_ids
+
+        return fig
+
+    def filtered_histogram(self, x, **kwargs):
+        from .plotly_plots import Histogram
+
+        fig = Histogram(self.filtered_data, x, **kwargs)
+
+        fig.plotter = self
+
+        fig.layer = self
+        fig.ids = self.filtered_ids
 
         return fig
