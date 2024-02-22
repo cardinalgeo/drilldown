27880cc295924454fc0b9324fcb9ca9a8277ca04
Robert Collar <rscollar@gmail.com>
2024-02-22 17:24:48 -0500

    bring back `DrillHole` class; fix imports

--- drilldown/__init__.py
+++ drilldown/__init__.py
@@ -1,17 +1,13 @@
-from .holes import DrillHole, DrillHoleGroup, Intervals, Points, Collars, Surveys
+from .holes import Intervals, Points, Collars, Surveys
 from .plotter import DrillDownPlotter
 
-from .ui.ui import DrillDownTramePlotter, DrillDownPanelPlotter
-from .image.image import ImageViewer
+from .ui.ui import DrillDownTramePlotter
 
 __all__ = [
-    "DrillHole",
-    "DrillHoleGroup",
     "Intervals",
     "Points",
     "Collars",
     "Surveys",
     "DrillDownPlotter",
-    "DrillDownPanelPlotter",
     "DrillDownTramePlotter",
 ]
--- drilldown/holes.py
+++ drilldown/holes.py
@@ -406,9 +406,9 @@ class Intervals(HoleData):
 
             self.from_depths_desurveyed[hole_filter] = from_depths_desurveyed
             self.to_depths_desurveyed[hole_filter] = to_depths_desurveyed
-            self.intermediate_depths_desurveyed[
-                hole_filter
-            ] = intermediate_depths_desurveyed
+            self.intermediate_depths_desurveyed[hole_filter] = (
+                intermediate_depths_desurveyed
+            )
 
     def make_mesh(self):
         meshes = None
@@ -615,3 +615,344 @@ class Surveys:
             p.add_collars(self.collars)
 
         return p.show()
+
+
+class DrillHole:
+    def __init__(self):
+        self.workspace = Workspace()
+        self.hole_group = DrillholeGroup.create(self.workspace)
+        self.vars = []
+        self.categorical_interval_vars = []
+        self.continuous_interval_vars = []
+        self.categorical_point_vars = []
+        self.continuous_point_vars = []
+
+        self.intervals = {}
+        self.points = {}
+
+        self.cat_to_code_map = {}
+        self.code_to_cat_map = {}
+        self.code_to_color_map = {}
+        self.cat_to_color_map = {}
+        self.matplotlib_formatted_color_maps = {}
+
+    def add_collar(self, collar):
+        if isinstance(collar, pd.core.series.Series) | isinstance(
+            collar, pd.core.frame.DataFrame
+        ):
+            collar = collar.values[0]
+
+        self.collar = collar
+
+    def add_survey(self, dist, azm, dip):
+        if isinstance(dist, pd.core.series.Series):
+            dist = dist.values
+        if isinstance(azm, pd.core.series.Series):
+            azm = azm.values
+        if isinstance(dip, pd.core.series.Series):
+            dip = dip.values
+
+        self.survey = np.c_[dist, azm, dip]
+        self._create_hole()
+
+    def _create_hole(self):
+        self._hole = Drillhole.create(
+            self.workspace,
+            collar=self.collar,
+            surveys=self.survey,
+            name="",
+            parent=self.hole_group,
+        )
+
+    def _add_from_to(self, from_to):
+        if isinstance(from_to, pd.core.frame.DataFrame):
+            from_to = from_to.values
+        self.from_to = from_to.astype(np.float64)
+
+        return self.from_to
+
+    def _make_from_to(self, depths, connected=True):
+        if connected == True:
+            from_depths = depths[:-1]
+            to_depths = depths[1:]
+
+            if depths.ndim == 1:
+                depths = np.empty([from_depths.shape[0], 2])
+            else:
+                depths = np.empty([from_depths.shape[0], 2, from_depths.shape[1]])
+
+            depths[:, 0] = from_depths
+            depths[:, 1] = to_depths
+
+            return depths
+
+    def _add_data(self, data, name=None):
+        self.cat_to_code_map[name] = data.cat_to_code_map
+        self.code_to_cat_map[name] = data.code_to_cat_map
+        self.code_to_color_map[name] = data.code_to_color_map
+        self.cat_to_color_map[name] = data.cat_to_color_map
+        self.matplotlib_formatted_color_maps[name] = (
+            data.matplotlib_formatted_color_maps
+        )
+
+    def add_intervals(self, intervals, name="intervals"):
+        self.intervals[name] = intervals
+        self.categorical_interval_vars += intervals.categorical_vars
+        self.continuous_interval_vars += intervals.continuous_vars
+        self.vars += intervals.vars_all
+        self._add_data(intervals, name)
+
+    def add_points(self, points, name="points"):
+        self.points[name] = points
+        self.categorical_point_vars += points.categorical_vars
+        self.continuous_point_vars += points.continuous_vars
+        self.vars += points.vars_all
+        self._add_data(points, name)
+
+    def desurvey(self, depths=None):
+        if depths is None:
+            # return desurveyed survey depths if no depths passed
+            return self._hole.desurvey(self.survey[:, 0])
+        else:
+            return self._hole.desurvey(depths)
+
+    def _make_line_mesh(self, from_depth, to_depth):
+        """Make a mesh consisting of line segments for which a connected topology is assumed."""
+        depths = np.empty((from_depth.shape[0] + to_depth.shape[0], 3))
+        depths[0::2, :] = from_depth
+        depths[1::2, :] = to_depth
+        n_connected = np.ones(int(depths.shape[0] / 2), dtype="int") * 2
+        from_positions = np.arange(0, depths.shape[0] - 1, 2)
+        to_positions = np.arange(1, depths.shape[0], 2)
+        depth_connectivity = np.hstack(
+            np.stack([n_connected, from_positions, to_positions], axis=1)
+        )
+        mesh = pv.PolyData(depths, lines=depth_connectivity)
+
+        return mesh
+
+    def make_collar_mesh(self):
+        mesh = pv.PolyData(self.collar)
+        mesh["hole ID"] = self._hole.name
+
+        return mesh
+
+    def make_survey_mesh(self):
+        depths = self.desurvey()
+        from_to = self._make_from_to(depths)
+        mesh = self._make_line_mesh(from_to[0], from_to[1])
+
+        return mesh
+
+    def make_intervals_mesh(self, name=None):
+        if name is None:
+            name = list(self.intervals.keys())[0]
+
+        intervals = self.intervals[name]
+        from_to = intervals.depths
+        from_depths = self.desurvey(from_to[:, 0])
+        to_depths = self.desurvey(from_to[:, 1])
+        intermediate_depths = np.mean([from_depths, to_depths], axis=0)
+        mesh = self._make_line_mesh(from_depths, to_depths)
+
+        mesh.cell_data["from"] = from_to[:, 0]
+        mesh.cell_data["to"] = from_to[:, 1]
+        mesh.cell_data["x"] = intermediate_depths[:, 0]
+        mesh.cell_data["y"] = intermediate_depths[:, 1]
+        mesh.cell_data["z"] = intermediate_depths[:, 2]
+        self.continuous_interval_vars += ["from", "to", "x", "y", "z"]
+        for var in intervals.vars_all:
+            data = intervals.data[var]["values"]
+            _type = intervals.data[var]["type"]
+            if _type == "str":
+                mesh[var] = data
+            else:
+                mesh.cell_data[var] = data
+
+        return mesh
+
+    def make_points_mesh(self, name=None):
+        if name is None:
+            name = list(self.points.keys())[0]
+
+        points = self.points[name]
+        depths = points.depths
+        depths = self.desurvey(depths)
+        mesh = pv.PolyData(depths)
+        for var in points.vars_all:
+            data = points.data[var]["values"]
+            _type = points.data[var]["type"]
+            if _type == "str":
+                mesh[var] = data
+            else:
+                mesh.point_data[var] = data
+
+        return mesh
+
+    def show_collar(self, *args, **kwargs):
+        collar_mesh = self.make_collar_mesh()
+        p = DrillDownPlotter()
+        p.add_collars(collar_mesh, *args, **kwargs)
+
+        return p.show()
+
+    def show_survey(self, show_collar=False, *args, **kwargs):
+        survey_mesh = self.make_survey_mesh()
+        p = DrillDownPlotter()
+        p.add_surveys(survey_mesh, *args, **kwargs)
+
+        if show_collar == True:
+            collar_mesh = self.make_collar_mesh()
+            p.add_collars(collar_mesh)
+
+        return p.show()
+
+    def show_intervals(
+        self, name=None, show_collar=False, show_survey=False, *args, **kwargs
+    ):
+        if name is None:
+            name = list(self.intervals.keys())[0]
+
+        intervals_mesh = self.make_intervals_mesh(name)
+        p = DrillDownPlotter()
+        p.add_intervals(
+            intervals_mesh,
+            name,
+            self.categorical_interval_vars,
+            self.continuous_interval_vars,
+            *args,
+            **kwargs,
+        )
+
+        if show_collar == True:
+            collar_mesh = self.make_collar_mesh()
+            p.add_collars(collar_mesh)
+
+        if show_survey == True:
+            survey_mesh = self.make_survey_mesh()
+            p.add_surveys(survey_mesh)
+
+        return p.show()
+
+    def show_points(
+        self, name=None, show_collar=False, show_survey=False, *args, **kwargs
+    ):
+        if name is None:
+            name = list(self.points.keys())[0]
+
+        points_mesh = self.make_points_mesh(name)
+        p = DrillDownPlotter()
+        p.add_points(
+            points_mesh,
+            name,
+            self.categorical_point_vars,
+            self.continuous_point_vars,
+            *args,
+            **kwargs,
+        )
+
+        if show_collar == True:
+            collar_mesh = self.make_collar_mesh()
+            p.add_collars(collar_mesh)
+
+        if show_survey == True:
+            survey_mesh = self.make_survey_mesh()
+            p.add_surveys(survey_mesh)
+
+        return p.show()
+
+    def show(self):
+        collar_mesh = self.make_collar_mesh()
+        survey_mesh = self.make_survey_mesh()
+        intervals_mesh = self.make_intervals_mesh()
+        points_mesh = self.make_points_mesh()
+
+        p = DrillDownPlotter()
+        p.add_collars(collar_mesh)
+        p.add_surveys(survey_mesh)
+
+        intervals_name = list(self.intervals.keys())[0]
+        p.add_intervals(
+            intervals_mesh,
+            intervals_name,
+            self.categorical_interval_vars,
+            self.continuous_interval_vars,
+            radius=10,
+        )
+
+        points_name = list(self.points.keys())[0]
+        p.add_points(
+            points_mesh,
+            points_name,
+            self.categorical_point_vars,
+            self.continuous_point_vars,
+        )
+
+        return p.show()
+
+    def drill_log(self, log_vars=[]):
+        if self.intervals or self.points:  # ensure there is data to plot
+            log = DrillLog()
+            if len(log_vars) == 0:
+                interval_vars = (
+                    self.categorical_interval_vars + self.continuous_interval_vars
+                )
+                point_vars = self.categorical_point_vars + self.continuous_point_vars
+                log_vars = interval_vars + point_vars
+
+            for var in log_vars:
+                for name in self.intervals.keys():
+                    intervals = self.intervals[name]
+                    from_to = intervals.depths
+                    if var in intervals.categorical_vars:
+                        cat_to_color_map = self.cat_to_color_map[name]
+                        values = intervals.data[var]["values"]
+                        values = np.array(
+                            [self.code_to_cat_map[name][var][val] for val in values]
+                        )
+                        log.add_categorical_interval_data(
+                            var,
+                            from_to,
+                            values,
+                            cat_to_color_map.get(var, None),
+                        )
+
+                        exit_flag = True
+                        break
+
+                    elif var in intervals.continuous_vars:
+                        values = intervals.data[var]["values"]
+
+                        log.add_continuous_interval_data(var, from_to, values)
+
+                        exit_flag = True
+                        break
+
+                    if exit_flag == True:
+                        break
+
+                for name in self.points.keys():
+                    points = self.points[name]
+                    depths = points.depths
+                    if var in points.categorical_vars:
+                        cat_to_color_map = self.cat_to_color_map[name]
+                        values = points.data[var]["values"]
+                        values = np.array(
+                            [self.code_to_cat_map[name][var][val] for val in values]
+                        )
+                        log.add_categorical_point_data(
+                            var, depths, values, cat_to_color_map.get(var, None)
+                        )
+
+                        exit_flag = True
+                        break
+
+                    elif var in self.continuous_point_vars:
+                        pass
+
+                    if exit_flag == True:
+                        break
+
+            log.create_figure(y_axis_label="Depth (m)")
+
+            return log.fig
